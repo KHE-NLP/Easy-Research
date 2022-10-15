a66645ad5cb2ecc6509d67448dc9e1ff84e10f6e
opfromthestart <opfromthestart@gmail.com>
2022-10-15 13:01:50 -0400

    Added main check to cohere

--- cohereGeneration.py
+++ cohereGeneration.py
@@ -1,5 +1,7 @@
 import cohere
-co = cohere.Client('hpaaYCC1MGPwyigl9JhSQg3NCZaLzDkSrYM6Iy6U')
-response = co.generate(prompt='Once upon a time in a magical land called')
 
-print('Prediction: {}'.format(response.generations[0].text))
+if __name__ == "__main__":
+    co = cohere.Client('hpaaYCC1MGPwyigl9JhSQg3NCZaLzDkSrYM6Iy6U')
+    response = co.generate(prompt='Once upon a time in a magical land called')
+
+    print('Prediction: {}'.format(response.generations[0].text))
