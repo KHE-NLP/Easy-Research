997a181dda24c3d39777ab585414fa115c255fec
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2022-10-15 12:34:46 -0400

    initial commit for cohere

---
+++ cohereGeneration.py
@@ -0,0 +1,5 @@
+import cohere
+co = cohere.Client('hpaaYCC1MGPwyigl9JhSQg3NCZaLzDkSrYM6Iy6U')
+response = co.generate(prompt='Once upon a time in a magical land called')
+
+print('Prediction: {}'.format(response.generations[0].text))
