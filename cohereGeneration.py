b759b34792427666b8ec7137b168d74931073bd6
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2022-10-15 14:52:05 -0400

    adding parser elements

--- cohereGeneration.py
+++ cohereGeneration.py
@@ -1,8 +1,28 @@
 import cohere
+from parsers import get_pdf_paragraphs
+
+paragraphs = get_pdf_paragraphs("https://arxiv.org/pdf/2210.06929.pdf")
+
+
+to_rem = []
+
+for p in paragraphs:
+	if((paragraphs.count(p)>1) or (p[0] == "[") or (len(p)<20)):
+		to.remove(p)
+	
+
+	
 
 co = cohere.Client('hpaaYCC1MGPwyigl9JhSQg3NCZaLzDkSrYM6Iy6U')
-responses = "Format: Title:, Author(s): Summarize the following passage for a presentation: \n ${} \n\n  Summary:"
+prompt = "Summarize the following passage for a presentation: \n ${fPassage} \n\n  Summary:"
+responses = []
+for p in paragraphs:
+	stopsequences = ['\n\n', '\t']
+	response = co.generate(prompt=prompt.format(fPassage=p), max_tokens=150, temperature=0.9, k=10)
+	responses.append(response)
+	print("Completed paragraph")
+	
+for e in responses:
+	print(e.generations[0].text)
 
-stopsequences = ['\n\n', '\t']
-response = co.generate(prompt=responses[0], max_tokens=150, temperature=0.9, k=10)
-print(response.generations[0].text)
+
