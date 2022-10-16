fe854c594b20bca2be3d9d1847800db9c24862dc
opfromthestart <opfromthestart@gmail.com>
2022-10-16 03:05:41 -0400

    Added full pdf parsing 7

--- pdf_handle.js
+++ pdf_handle.js
@@ -4,7 +4,35 @@ function uploadFile() {
     formData.append("data", blobFile);
 
     $.ajax({
-       url: "147.182.183.30:12345/pdffile",
+       url: "http://147.182.183.30:12345/pdffile",
+       type: "POST",
+       data: formData,
+       processData: false,
+       contentType: false,
+       success: function(response) {
+            let blob = new Blob([response], { type: "*/*" });
+
+            let a = document.createElement('a');
+            a.href = window.URL.createObjectURL(blob);
+            a.download = "flashread.pptx";
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            window.URL.removeObjectURL(a.href);
+       },
+       error: function(jqXHR, textStatus, errorMessage) {
+           console.log(errorMessage);
+       }
+    });
+}
+
+function uploadUrl() {
+    var data = $('#urlPDF')[0].text;
+    var formData = new FormData();
+    formData.append("data", data);
+
+    $.ajax({
+       url: "http://147.182.183.30:12345/pdffile",
        type: "POST",
        data: formData,
        processData: false,
